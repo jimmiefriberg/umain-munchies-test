ee1fd8fe07278a06756a75ac5f41a9e0ec092648
Jimmie Friberg <friberg.jimmie@gmail.com>
2025-05-20 17:23:39 +0200

    chore: import assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,36 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Muchies",
-  description: "Umain Tech Test",
+  description: "Umain Test Site",
 };
+
+export const viewport = {
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+  width: "device-width",
+};
+
+const SFPro = localFont({
+  src: [
+    {
+      path: "../../public/fonts/SF-Pro.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/SF-Pro-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  variable: "--font-sfpro",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +39,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={` antialiased`}>{children}</body>
+      <body className={`${SFPro.variable} antialiased font-sans`}>
+        {children}
+      </body>
     </html>
   );
 }
